ffe102f5f678ffe1d923d05e4c9178183f5d4c2f
김우성 <84122192+wooseongkim-29cm@users.noreply.github.com>
2023-11-10 23:35:59 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,12 +6,9 @@ import PackageDescription
 let package = Package(
   name: "FlexLayout",
   products: [
-    .library(name: "FlexLayout", targets: ["FlexLayout"]),
-    .library(name: "FlexLayoutYoga", targets: ["FlexLayoutYoga"]),
-    .library(name: "FlexLayoutYogaKit", targets: ["FlexLayoutYogaKit"])
-    .library(name: "FlexLayout-Dynamic", type: .dynamic,  targets: ["FlexLayout-Dynamic"]),
-    .library(name: "FlexLayoutYoga-Dynamic", type: .dynamic,  targets: ["FlexLayoutYoga-Dynamic"]),
-    .library(name: "FlexLayoutYogaKit-Dynamic", type: .dynamic,  targets: ["FlexLayoutYogaKit-Dynamic"])
+    .library(name: "FlexLayout", type: .dynamic, targets: ["FlexLayout"]),
+    .library(name: "FlexLayoutYoga", type: .dynamic, targets: ["FlexLayoutYoga"]),
+    .library(name: "FlexLayoutYogaKit", type: .dynamic, targets: ["FlexLayoutYogaKit"])
   ],
   dependencies: [
   ],
@@ -48,48 +45,6 @@ let package = Package(
     .target(
       name: "FlexLayoutYogaKit",
       dependencies: ["FlexLayoutYoga"],
-      path: "Sources/YogaKit",
-      cSettings: [
-        .define("FLEXLAYOUT_SWIFT_PACKAGE")
-      ],
-      cxxSettings: [
-        .define("FLEXLAYOUT_SWIFT_PACKAGE"),
-        .headerSearchPath("include/YogaKit/"),
-        .headerSearchPath("./")
-      ]
-    ),
-    .target(
-      name: "FlexLayout-Dynamic",
-      dependencies: ["FlexLayoutYoga-Dynamic", "FlexLayoutYogaKit-Dynamic"],
-      path: "Sources/Swift",
-      cSettings: [
-        .define("FLEXLAYOUT_SWIFT_PACKAGE")
-      ],
-      cxxSettings: [
-        .define("FLEXLAYOUT_SWIFT_PACKAGE"),
-        .headerSearchPath("include/yoga/"),
-        .headerSearchPath("./")
-      ],
-      swiftSettings: [
-        .define("FLEXLAYOUT_SWIFT_PACKAGE")
-      ]
-    ),
-    .target(
-      name: "FlexLayoutYoga-Dynamic",
-      dependencies: [],
-      path: "Sources/yoga",
-      cSettings: [
-        .define("FLEXLAYOUT_SWIFT_PACKAGE")
-      ],
-      cxxSettings: [
-        .define("FLEXLAYOUT_SWIFT_PACKAGE"),
-        .headerSearchPath("include/yoga/"),
-        .headerSearchPath("./")
-      ]
-    ),
-    .target(
-      name: "FlexLayoutYogaKit-Dynamic",
-      dependencies: ["FlexLayoutYoga-Dynamic"],
       path: "Sources/YogaKit",
       cSettings: [
         .define("FLEXLAYOUT_SWIFT_PACKAGE")
