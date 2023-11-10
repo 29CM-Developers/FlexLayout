ad925a9c42cec1b353690d8923ca19c2142db75a
김우성 <84122192+wooseongkim-29cm@users.noreply.github.com>
2023-11-10 23:16:43 +0900

    Package.swift 에서 Dynamic Framework 지원

--- Package.swift
+++ Package.swift
@@ -9,6 +9,9 @@ let package = Package(
     .library(name: "FlexLayout", targets: ["FlexLayout"]),
     .library(name: "FlexLayoutYoga", targets: ["FlexLayoutYoga"]),
     .library(name: "FlexLayoutYogaKit", targets: ["FlexLayoutYogaKit"])
+    .library(name: "FlexLayout-Dynamic", type: .dynamic,  targets: ["FlexLayout"]),
+    .library(name: "FlexLayoutYoga-Dynamic", type: .dynamic,  targets: ["FlexLayoutYoga"]),
+    .library(name: "FlexLayoutYogaKit-Dynamic", type: .dynamic,  targets: ["FlexLayoutYogaKit"])
   ],
   dependencies: [
   ],
@@ -45,6 +48,50 @@ let package = Package(
     .target(
       name: "FlexLayoutYogaKit",
       dependencies: ["FlexLayoutYoga"],
+      path: "Sources/YogaKit",
+      cSettings: [
+        .define("FLEXLAYOUT_SWIFT_PACKAGE")
+      ],
+      cxxSettings: [
+        .define("FLEXLAYOUT_SWIFT_PACKAGE"),
+        .headerSearchPath("include/YogaKit/"),
+        .headerSearchPath("./")
+      ]
+    ),
+
+
+    .target(
+      name: "FlexLayout-Dynamic",
+      dependencies: ["FlexLayoutYoga-Dynamic", "FlexLayoutYogaKit-Dynamic"],
+      path: "Sources/Swift",
+      cSettings: [
+        .define("FLEXLAYOUT_SWIFT_PACKAGE")
+      ],
+      cxxSettings: [
+        .define("FLEXLAYOUT_SWIFT_PACKAGE"),
+        .headerSearchPath("include/yoga/"),
+        .headerSearchPath("./")
+      ],
+      swiftSettings: [
+        .define("FLEXLAYOUT_SWIFT_PACKAGE")
+      ]
+    ),
+    .target(
+      name: "FlexLayoutYoga-Dynamic",
+      dependencies: [],
+      path: "Sources/yoga",
+      cSettings: [
+        .define("FLEXLAYOUT_SWIFT_PACKAGE")
+      ],
+      cxxSettings: [
+        .define("FLEXLAYOUT_SWIFT_PACKAGE"),
+        .headerSearchPath("include/yoga/"),
+        .headerSearchPath("./")
+      ]
+    ),
+    .target(
+      name: "FlexLayoutYogaKit-Dynamic",
+      dependencies: ["FlexLayoutYoga-Dynamic"],
       path: "Sources/YogaKit",
       cSettings: [
         .define("FLEXLAYOUT_SWIFT_PACKAGE")
